ccd46063029cdb6e3321a368e7827977f649d787
Rico <richardthomaswhitfield@gmail.com>
2017-02-26 15:18:25 -0600

    part1

--- basic-react/src/js/client.js
+++ basic-react/src/js/client.js
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Layout from "./components/Layout";
+class Layout extends React.Component {
+	render() {
+		return (
+			<h1>It Worked!</h1>
+		);
+	}
+}
 
 const app = document.getElementById('app');
 ReactDOM.render(<Layout/>, app);
