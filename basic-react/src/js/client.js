1cf54fcab3e9ed6a735d4446692cf1e0a5e6ac08
Rico <richardthomaswhitfield@gmail.com>
2017-02-26 16:08:29 -0600

    part3

--- basic-react/src/js/client.js
+++ basic-react/src/js/client.js
@@ -1,18 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class Layout extends React.Component {
-
-	constructor() {
-		super();
-		this.name = "Richard";
-	}
-	render() {
-		return (
-			<h1>It Worked, {this.name}!</h1>
-		);
-	}
-}
+import Layout from "./components/Layout";
 
 const app = document.getElementById('app');
 ReactDOM.render(<Layout/>, app);
